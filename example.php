1b642a1d4b5b0d5a9ca681d05abc90aa4da29f6f
Tomasz Sawicki <falundir@gmail.com>
2011-07-15 16:50:12 +0000

    more usage description

--- example.php
+++ example.php
@@ -9,12 +9,12 @@ kyBase::init("http://mykayako.example.com/api/index.php", "<API key>", "<Secret 
 
 /**
  * Optional. Setting defaults for new tickets.
+ * WARNING:
  * Object identifiers may be different in your installation.
- * To find out proper identifier fetch all objects with:
+ * To find out proper identifier fetch all objects and examine them with:
  * kyTicketStatus::getAll()
  * kyTicketPriority::getAll()
  * kyTicketType::getAll()
- * and examine them.
  */
 
 $default_status_id = 1;
@@ -26,10 +26,25 @@ kyTicket::setDefaults($default_status_id, $default_priority_id, $default_type_id
  * Creating ticket.
  */
 
-//load department, user and staff
-$department = kyDepartment::get(1);
-$user = kyUser::get(1);
-$staff = kyStaff::get(1);
+/**
+ * Load the department, user and staff.
+ * WARNING:
+ * Object identifiers may be different in your installation.
+ * To find out proper identifier fetch all objects and examine them with:
+ * kyDepartment::getAll()
+ * kyUser::getAll()
+ * kyStaff::getAll()
+ * Also make sure that:
+ * - the user has the right to create ticket in the department,
+ * - the staff user has right to be assigned to tickets in the department,
+ * - department type is Public and it's module is set to "Tickets".
+ */
+$department_id = 1;
+$user_id = 1;
+$staff_id = 1;
+$department = kyDepartment::get($department_id);
+$user = kyUser::get($user_id);
+$staff = kyStaff::get($staff_id);
 
 //create ticket
 $new_ticket = $user->newTicket($department,
@@ -37,47 +52,39 @@ $new_ticket = $user->newTicket($department,
 Aliquam placerat cursus augue sed adipiscing. Proin viverra egestas nulla et sollicitudin.',
 	'Lorem ipsum 1')->setOwnerStaff($staff)->create();
 
-$ticket_id = $new_ticket->getId();
+//print the ticket Display Identifier
+print "The ticket was created and its ID is: ".$new_ticket->getDisplayId();
 
 /**
  * Changing ticket.
  */
 
-//load ticket
-$ticket = kyTicket::get($ticket_id);
-
 //change status
-$ticket->setStatusId(5)->update();
+$new_ticket->setStatusId(5)->update();
 
 /**
  * Adding ticket post (as staff).
  */
 
-//load ticket
-$ticket = kyTicket::get($ticket_id);
-
 //add new post
-$new_ticket_post = $ticket->newPost($ticket->getOwnerStaff(), 'What??')->create();
+$new_ticket_post = $new_ticket->newPost($new_ticket->getOwnerStaff(), 'What??')->create();
 
 /**
  * Adding ticket post with attachment (as user).
  */
 
-//load ticket
-$ticket = kyTicket::get($ticket_id);
-
 //add new post
-$new_ticket_post = $ticket->newPost($ticket->getUser(), 'Sorry, I forgot the attachment...')->create();
+$new_ticket_post = $new_ticket->newPost($new_ticket->getUser(), 'Sorry, I forgot the attachment...')->create();
 
-//add attachment to the post
+//add attachment to the post - change the path to the proper file
 $new_ticket_attachment = $new_ticket_post->newAttachmentFromFile('/path/to/file.pdf')->create();
 
 /**
  * Searching for tickets (using helper).
  */
-$tickets = kySearchTicket::createNew()->addDepartmentId(3)->addOwnerStaffId(3)->addOwnerStaffId(4)->search();
+$tickets = kySearchTicket::createNew()->addDepartmentId(1)->addTicketStatusId(2)->addTicketStatusId(3)->addOwnerStaffId(4)->addUser(3)->search();
 
 /**
  * Searching for tickets (using getAll).
  */
-$tickets = kyTicket::getAll(array(1), array(5), array(1), array(1));
+$tickets = kyTicket::getAll(array(1), array(2, 3), array(4), array(3));
