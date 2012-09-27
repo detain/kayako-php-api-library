0c172edf2b9356840401684d5f42e88be2ce9c4a
Tomasz Sawicki <falundir@gmail.com>
2012-09-27 17:27:34 +0000

    - support for ticket post isprivate field (fixes #102)

--- kyTicketPost.php
+++ kyTicketPost.php
@@ -4,7 +4,7 @@
  *
  * @author Tomasz Sawicki (https://github.com/Furgas)
  * @link http://wiki.kayako.com/display/DEV/REST+-+TicketPost
- * @since Kayako version 4.01.240
+ * @since Kayako version 4.50.1636
  * @package Object\Ticket
  *
  * @noinspection PhpDocSignatureInspection
@@ -183,6 +183,13 @@ class kyTicketPost extends kyObjectBase {
 	 */
 	protected $subject;
 
+    /**
+     * Whether the ticket post should be created as private (hidden from the customer) or not.
+     * @apiField
+     * @var bool
+     */
+    protected $is_private = false;
+
 	/**
 	 * Ticket post attachments.
 	 * @var kyTicketAttachment[]
@@ -227,6 +234,12 @@ class kyTicketPost extends kyObjectBase {
 		$this->staff_id = ky_assure_positive_int($data['staffid']);
 		$this->is_survey_comment = ky_assure_bool($data['issurveycomment']);
 		$this->contents = $data['contents'];
+        //isprivate field is not returned when getting all ticket posts (it may be a bug in Kayako)
+        if (array_key_exists('isprivate', $data)) {
+		    $this->is_private = ky_assure_bool($data['isprivate']);
+        } else {
+            $this->is_private = null;
+        }
 	}
 
 	public function buildData($create) {
@@ -237,6 +250,7 @@ class kyTicketPost extends kyObjectBase {
 		$data['ticketid'] = $this->ticket_id;
 		$data['subject'] = $this->subject !== null ? $this->subject : '';
 		$data['contents'] = $this->contents;
+        $data['isprivate'] = $this->is_private;
 
 		if (!is_numeric($this->staff_id) && !is_numeric($this->user_id))
 			throw new kyException("Value for API fields 'staffid' or 'userid' is required for this operation to complete.");
@@ -674,6 +688,28 @@ class kyTicketPost extends kyObjectBase {
 	 */
 	public function setContents($contents) {
 		$this->contents = ky_assure_string($contents);
+		return $this;
+	}
+
+    /**
+     * Returns whether the ticket post was created as private (hidden from the customer) or not.
+     *
+     * @return bool
+     * @filterBy
+     * @orderBy
+     */
+    public function getIsPrivate() {
+        return $this->is_private;
+    }
+
+	/**
+	 * Sets whether the ticket post should be created as private (hidden from the customer) or not.
+	 *
+	 * @param bool $is_private Whether the ticket post should be created as private (hidden from the customer) or not.
+	 * @return kyTicketPost
+	 */
+	public function setIsPrivate($is_private) {
+		$this->is_private = ky_assure_bool($is_private);
 		return $this;
 	}
 
